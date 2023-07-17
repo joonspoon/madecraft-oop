7b5f16b64b6643729da8cf9527dfbc0de3b860f9
Ariel Wang <arielxiewang@gmail.com>
2023-07-17 15:40:06 +1000

    Inheritance

---
+++ 5.Inheritance/examples/Random.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class Random {
+	
+	public static void main(String []arg) {
+		ExtraRandom extraRandom = new ExtraRandom();
+		
+		System.out.println(extraRandom.nextLetter());
+	}
+
+}
